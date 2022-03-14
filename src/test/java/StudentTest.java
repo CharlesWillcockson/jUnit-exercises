da9b192b03df2f2eefa8bb16e18cf19903d1be3b
Charles Willcockson <willcockson.charles@gmail.com>
2022-03-14 14:47:56 -0500

    finished tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,32 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 public class StudentTest {
+
+    @Test
+    public void constructorTest() {
+        Student student1 = new Student("Charles", 2345667);
+        assertEquals("Charles", student1.getName());
+        assertEquals(2345667, student1.getId());
+    }
+
+    @Test
+    public void addGradeTest() {
+        Student student1 = new Student("Charles", 2345667);
+        student1.addGrade(98);
+        ArrayList<Integer> grades = new ArrayList<>();
+        grades.add(98);
+        assertEquals(student1.getGrades(), grades);
+    }
+
+    @Test
+    public void getGradeAverageTest() {
+        Student student1 = new Student("Charles", 2345667);
+        student1.addGrade(90);
+        student1.addGrade(100);
+        assertEquals(student1.getGradeAverage(), 95, 0);
+    }
 }
